8426691957bb2c5a21910f76c504f9530f9d261a
R3AL <r3alprox@gmail.com>
2026-09-02 07:08:49 +0530

    feat: unify containment across attack vectors

--- core/antinuke/vectorContainment.js
+++ core/antinuke/vectorContainment.js
@@ -1,1 +1,12 @@
-const { containMember }=require("./actions");const { quarantineMember }=require("../quarantine/manager");const { triggerPanic }=require("../panic/manager");async function containVector(guild,executorId,reason,meta={}){const containment=await containMember(guild,executorId,reason);const quarantine=await quarantineMember(guild,executorId,reason).catch(e=>({ok:false,message:e.message}));if(meta.critical)await triggerPanic(guild,reason,{executorId,...meta});return{containment,quarantine};}module.exports={containVector};
+const { containMember }=require("./actions");
+const { quarantineMember }=require("../quarantine/manager");
+const { triggerPanic }=require("../panic/manager");
+const { takeSnapshot }=require("../backups/snapshot");
+async function containVector(guild,executorId,reason,meta={}){
+ takeSnapshot(guild);
+ const containment=await containMember(guild,executorId,reason).catch(e=>({ok:false,message:e.message}));
+ const quarantine=await quarantineMember(guild,executorId,reason).catch(e=>({ok:false,message:e.message}));
+ if(meta.critical) await triggerPanic(guild,reason,{executorId,...meta});
+ return{containment,quarantine};
+}
+module.exports={containVector};
