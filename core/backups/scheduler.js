b891594dafb2f3c5da34cefd46ff0aa0118a1f04
R3AL <r3alprox@gmail.com>
2026-09-03 21:41:21 +0530

    Prevent backup scheduler failures from crashing bot

--- core/backups/scheduler.js
+++ core/backups/scheduler.js
@@ -1,4 +1,9 @@
 const {takeSnapshot}=require("./snapshot");
 const timers=new Map();
-function startBackupScheduler(client,intervalMs=3*60*60*1000){for(const guild of client.guilds.cache.values())takeSnapshot(guild);if(timers.has("main"))clearInterval(timers.get("main"));timers.set("main",setInterval(()=>{for(const guild of client.guilds.cache.values())takeSnapshot(guild);},intervalMs));}
+function safeSnapshot(guild){try{return takeSnapshot(guild);}catch(error){console.error("Backup snapshot failed for guild "+guild.id+":",error);return null;}}
+function startBackupScheduler(client,intervalMs=3*60*60*1000){
+ for(const guild of client.guilds.cache.values())safeSnapshot(guild);
+ if(timers.has("main"))clearInterval(timers.get("main"));
+ timers.set("main",setInterval(()=>{for(const guild of client.guilds.cache.values())safeSnapshot(guild);},intervalMs));
+}
 module.exports={startBackupScheduler};
